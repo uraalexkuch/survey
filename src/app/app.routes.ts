fa84cbec4c28e7fde9ba212e84079a9b1e08e926
uraalexkuch <57583435+uraalexkuch@users.noreply.github.com>
2024-12-07 02:14:33 +0200

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import {SurveyComponent} from './survey/survey.component';
 import {CreatorComponent} from './creator/creator.component';
+import {NonauthComponent} from './nonauth/nonauth.component';
 
 export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'nonauth' },
+  { path: 'nonauth', component: NonauthComponent , data: {breadcrumb: 'Опитування-не авторизовано'}},
   { path: ':id', component: SurveyComponent , data: {breadcrumb: 'Опитування'}},
 
   {path: 'create', component: CreatorComponent, data: {breadcrumb: 'Конструктор'}},
